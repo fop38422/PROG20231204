3f80629ad6d1fcbed70348968f793f53237a1da6
angel <fop38422@educastur.es>
2023-12-04 20:50:40 +0100

    examen angel

---
+++ src/examen/Ejercicio1.java
@@ -0,0 +1,18 @@
+package examen;
+
+import java.util.Scanner;
+
+/*Define en una clase llamada Ejercicio1 un método estático que acepte un String y, 
+ * sin usar sentencias repetitivas, retorne otro String como resultado de sustituir en el primero 
+ * todos los caracteres por asteriscos, excepto el primero y el último.*/
+
+public class Ejercicio1 {
+	
+	static void encriptador() {
+		System.out.println("Introduce una palabra: ");
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
